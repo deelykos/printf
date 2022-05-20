d2607c674f989059bd9ef9c9d42dbf0d207ab89c
okjazzy <101530315+okjazzy@users.noreply.github.com>
2022-05-20 22:47:53 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,33 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
-
 /**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: The function associated
+ *struct print - structure for printing various types
+ *@k: type to print
+ *@a: function to print
  */
-
-typedef struct specifiers
+typedef struct print
 {
-	char specifiers;
-	int (*f)(va_list);
-} specifiers_t;
+        char *k;
+        int (*a)(va_list);
+} print_k;
 
-/*prototypes*/
-int _printf(const char *format, ...);
-int get_function(char s, va_list args);
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-/*Conversion specifiers*/
-int print_char(va_list args);
-int print_string(va_list args);
-int print_digit(va_list args);
-int print_mod(va_list args);
-int print_rev_string(va_list args);
-
-#endif
+#endif  /* _MAIN_H */
