6455c10de33383ff09fa68a3ef508bebc4067453
okjazzy <ibukunokiti@gmail.com>
2022-05-20 08:13:53 +0100

    Team Task

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 int _putchar(char c);
-int printf(const char *format, ...);
+int _printf(const char *format, ...);
 int print_char(va_list l);
 int print_string(va_list l);
 int print_int(va_list l);
@@ -13,7 +13,7 @@ int print_bin(va_list l);
 int print_unsign(va_list l);
 int print_octal(va_list l);
 int print_hex(va_list l);
-int print_hex_big(va_list l);
+int print_HEX(va_list l);
 int print_rot13(va_list l);
 int print_rev(va_list l);
 int print_percent(va_list l);
@@ -22,7 +22,7 @@ int get_flag(char s);
 void print_number(int n);
 int count_digit(int i);
 char *convert(unsigned long int num, int base, int lowercase);
-int print_string(va_list l);
+int print_SRN(va_list l);
 int _puts(char *str);
 
 /**
